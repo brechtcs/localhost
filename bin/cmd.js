31d0df0efdd5a0db28d6f97a43d93064ab037e54
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-02-22 22:51:33 +0100

    Make nginx optional

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,4 +3,26 @@
 const minimist = require('minimist')
 const pamphlets = require('../lib/pamphlets')
 
-pamphlets.start(minimist(process.argv))
+const argv = minimist(process.argv)
+const opts = {}
+
+if (argv.port || argv.p) {
+  opts.port = parseInt(argv.port || argv.p)
+}
+
+if (argv.nginx || argv.n) {
+  opts.nginx = [argv.nginx || argv.n].reduce(function (list, item) {
+    if (Array.isArray(item)) {
+      list = list.concat(item)
+    }
+    else if (typeof item === 'string') {
+      list = list.concat([item])
+    }
+    else {
+      list = list.concat(null)
+    }
+    return list
+  }, [])
+}
+
+pamphlets.start(opts)
